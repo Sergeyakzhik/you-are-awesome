5e6a217860b2c934a5d6904baeb9e0e590a82752
Sergeyakzhik <sergeyakzhik@gmail.com>
2018-03-09 12:22:30 +0300

    Com 12

--- src/index.js
+++ src/index.js
@@ -1,18 +1,81 @@
-// DO WHATEVER YOU WANT HERE
+const createEnumerableProperty = name => name;
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createNotEnumerableProperty = name1 => {
+    Object.defineProperty(Object.prototype, name1, {
+        enumerable: false,
+        set: function (value) {
+            Object.name1 = value;
+        },
+        get: function () {
+            return Object.name1;
+        }
+    });
+    return name1;
+};
 
-// return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+Function.prototype.valueOf = function () {
+    return this.i;
+};
+
+const createProtoMagicObject = () => {
+    return Function;
+};
+
+const incrementor = () => {
+    incrementor.i++;
+    return incrementor;
+};
+
+incrementor.i = 0;
+
+const asyncIncrementor = () => {
+    asyncIncrementor.i++;
+    return asyncIncrementor;
+};
+
+asyncIncrementor.i = 0;
+
+const createIncrementer = () => {
+    function* gen() {
+        var v = 0;
+        for(let j = 0; j < 3; j++) {
+            v++;
+            yield v;
+        }
+    }
+    return gen();
+};
+
+const returnBackInSecond = (x) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x);
+        }, 1000);
+    });
+};
+
+var number = 0;
+
+const getDeepPropertiesCount = (obj) => {
+    let ind = number;
+    for(let i in obj){
+        if (obj.hasOwnProperty(i)) {
+            number++;
+            getDeepPropertiesCount(obj[i]);
+        }
+    }
+    return number - ind;
+};
+
+const createSerializedObject = () => {
+    return Object(JSON.stringify(this));
+};
+
 const toBuffer = () => {};
-const sortByProto = () => {};
+
+const sortByProto = (obj) => {
+    return obj.sort();
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
